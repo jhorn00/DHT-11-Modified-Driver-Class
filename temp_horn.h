fc26fe38bc4b82457907638c72cf192e2106deaf
James Horn <58475047+jhorn00@users.noreply.github.com>
2021-10-22 15:34:26 -0400

    Add files via upload

--- temp_horn.h
+++ temp_horn.h
@@ -1,3 +1,4 @@
+//Edited by James Horn - Anything I didn't comment on I did not write.
 #pragma once
 #include <wiringPi.h>
 #include <stdio.h>
@@ -7,18 +8,20 @@
 #define MAXTIMINGS	85
 #define DHTPIN		7
 
-bool read_dht11_dat(std::vector<int>& dht11_dat)
+bool read_dht11_dat(std::vector<int>& dht11_dat) //add outvector param
 {
-	if (dht11_dat.size() != 6) {
+	if (dht11_dat.size() != 6) { //check container size
 		printf("DHT-11 Error: Invalid container size\n");
+		//failure
 		return false;
 	}
+
 	uint8_t laststate = HIGH;
 	uint8_t counter = 0;
 	uint8_t j = 0, i;
 	float	f;
 
-	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = dht11_dat[5] = 0;
+	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = dht11_dat[5] = 0; //initialize vector values to 0
 
 	pinMode(DHTPIN, OUTPUT);
 	digitalWrite(DHTPIN, LOW);
@@ -47,7 +50,7 @@ bool read_dht11_dat(std::vector<int>& dht11_dat)
 		if ((i >= 4) && (i % 2 == 0))
 		{
 			dht11_dat[j / 8] <<= 1;
-			if (counter > 50)
+			if (counter > 50) //changed from 16 to 50 as advised by a forum user
 				dht11_dat[j / 8] |= 1;
 			j++;
 		}
@@ -57,15 +60,14 @@ bool read_dht11_dat(std::vector<int>& dht11_dat)
 		(dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)))
 	{
 		f = dht11_dat[2] * 9. / 5. + 32;
-		//printf("Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
-		//	dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f);
+		//store fahrenheit data
 		dht11_dat[4] = f;
 		dht11_dat[5] = (f - dht11_dat[4]) * 10;
 		//success
 		return true;
 	}
 	else {
-		//printf("Data not good, skip\n");
+		//set to zero to avoid misleading temp values
 		dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = dht11_dat[5] = 0;
 		//failure
 		return false;
